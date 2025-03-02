1b3c4d6cb2d7740afbf99a366f570f968c0b7b83
SharmadG <127288299+SharmadG@users.noreply.github.com>
2025-03-02 13:30:59 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   01_basics/02_variables.js

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,20 @@
+"use strict"; // this treats your code as per newer standards. we don't actually need to write this now a days because it is automatically implemented.
+// alert (3+3); // this line cannot be executed in nodejs. Can be used only in browser.
+
+let name = "Sharmad"
+let age = 23
+let isLoggedIn = false
+
+/*
+Types of Datetypes (Primitive):
+number => 2 to power 53
+bigint
+string => "" // string can also be written in single quotes
+boolean => true/false
+null => standalone value
+undefined => this means the values is not definde yet
+symbol => for defining uniqueness of a component.
+
+Types of Datetypes (Non-Primitive):
+object
+*/
