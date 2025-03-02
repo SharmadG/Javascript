6c6a96afe0137f1d74ac702595f9a0b484081311
SharmadG <127288299+SharmadG@users.noreply.github.com>
2025-03-02 11:27:20 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   01_basics/01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,4 +3,11 @@ let accEmail = "abc@gmail.com"
 var accPass = "12345"
 accCity = "pune"
 
-accId = 3
+// accId = 3 // Not allowed - cannot re-initialize a const
+accEmail = "abc@sharmad.com"
+accPass = "223344"
+accCity = "pTown"
+
+console.log(accId);
+
+console.table([accId, accEmail, accPass, accCity]);
