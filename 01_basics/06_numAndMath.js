0122eed56fbb8c43e5bae9c38a7d524ad09ec3b0
SharmadG <127288299+SharmadG@users.noreply.github.com>
2025-03-04 13:58:37 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   01_basics/06_numAndMath.js

---
+++ 01_basics/06_numAndMath.js
@@ -0,0 +1,26 @@
+//+++++++++++++++++++++++++++++++++++ Number in javascript
+
+// const score = 400;
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 20.688;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); //provides values as per indian standards, and if not parameter is entered then consider US standard.
+
+//++++++++++++++++++++++++++++++++++++ MATHS - library in javascript is provided by default.
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.2)); // while having a decimal number, ceil print the next number even if decimal is less than 0.5
+console.log(Math.floor(4.9)); // while having a decimal number, floor print the current number if decimal is more than 0.5
+console.log(Math.min(34, 23, 56, 89)); // returns the minimum number
+console.log(Math.max(34, 23, 56, 89)); // returns the maximum number
